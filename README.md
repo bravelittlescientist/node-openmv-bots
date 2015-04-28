219f9badbbe9754cfcf7f52655b4b72b9ef1aa02
Eugenia Gabrielova <genia.likes.science@gmail.com>
2015-04-27 22:48:29 -0700

    logs in OpenSimulator client

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # Open-MV bots
 
 Node.js 3D virtual world client bots inspired by [LibOpenMetaverse](https://github.com/openmetaversefoundation/libopenmetaverse)
+
+# Dependencies
+
+This code relies on a running instance of [OpenSimulator](http://opensimulator.org/). The default configuration assumes a local grid is running.
+
+# Getting Started
+
+At minimum, this code requires a running instance of OpenSimulator and an existing user account. Update the example config file to contain your credentials.
+
+    $ cp config.json.example config.json
+
+Take a look at config.json
+
+```json
+{
+  "credentials": {
+    "first_name": "first_name",
+    "last_name": "last_name",
+    "passwd": "moonmoon"
+  },
+  "login_uri": "127.0.0.1:8002",
+  "start": "last"
+}
+```
+
+Update your login credentials and settings (name, login server, start position). Here is [login protocol documentation](http://opensimulator.org/wiki/SimulatorLoginProtocol).
+
+```json
+{
+  "credentials": {
+    "first_name": "Hermione",
+    "last_name": "Granger",
+    "passwd": "t1m3turn3r!"
+  },
+  "login_uri": "127.0.0.1:8002",
+  "start": "last"
+}
+```
+
+Now, run the login script.
+
+    $ node index.js
---
+++ index.js
@@ -0,0 +1,64 @@
+var md5 = require('MD5');
+var xmlrpc = require('xmlrpc');
+var macaddress = require('macaddress');
+var async = require('async');
+var nconf = require('nconf');
+
+// Login credentials
+nconf.file({file: './config.json'});
+nconf.defaults({
+  "login_uri": "http://127.0.0.1:8002", // Default login_uri for a local OpenSimulator grid 
+  "start": "last" // Can also be "home" or "uri:<region>&<x-coord>&<y-coord>&<z-coord>"
+});
+
+http://opensimulator.org/wiki/SimulatorLoginProtocol
+function get_login_parameters (first_name, last_name, passwd, mac_address) {
+    return {
+        "first": first_name,
+        "last": last_name,
+        "passwd": '$1$' + md5(passwd),
+        "mac": mac_address,
+        "start" : nconf.get('start'),
+        "version": "0.0.0",
+        "channel": "node-openmv-bots",
+        "platform": "Win",
+        "id0": "00000000-0000-0000-0000-000000000000",
+        "skipoptional": "true",
+        "last_exec_event": 0,
+        "options": []
+    };
+}
+
+function login() {
+    var xmlrpc_client = xmlrpc.createClient(nconf.get('login_uri'));
+    
+    async.auto({
+        get_mac_address: function getMacAddress(callback) {
+            macaddress.one(function(err, m) {
+                callback(err, m);
+            });
+        },
+
+        login_parameters: ['get_mac_address', function loginParameters(callback, results) {
+            var mac = results.get_mac_address;
+            var first = nconf.get('credentials:first_name');
+            var last = nconf.get('credentials:last_name');
+            var pw = nconf.get('credentials:passwd');
+            
+            callback(null, get_login_parameters(first, last, pw, mac));
+        }],
+
+        client_login: ['login_parameters', function clientLogin(callback, results) {
+            xmlrpc_client.methodCall('login_to_simulator', [results.login_parameters], function(error, value) {
+                callback(error, value);
+            });
+        }]
+    }, function (err, results) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(results);
+    });
+}
+
+login()
